0e065c6f435b455ef58f4a9cd9474bb862d744d8
the-code-machine <sarthak25ic049@satiengg.in>
2025-02-24 16:56:24 +0530

    Introduction demo #1

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <h1>Hii this is variable X</h1>
+    <h1>Hii this is variable X . We are here to provide you something new.
+
+    </h1>
   );
 }
 
