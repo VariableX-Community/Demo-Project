02b636b6242ae1226f1e78b729428558e62dddd2
Dev Jadiya <devjadiya15@gmail.com>
2025-02-25 22:52:49 +0530

    Added test

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <h1>Hii this is variable X . We are here to provide you something new.
+      Test
 
     </h1>
   );
